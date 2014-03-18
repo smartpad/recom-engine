cd5f729e325deb6e4dbcc4916ae6e0ff07b3d4b5
huybanh <huy.banh@jinnova.com>
2014-03-18 11:40:08 +0700

    pom dependency

---
+++ src/test/java/com/jinnova/smartpad/recom/FavoriteAlertGenerator.java
@@ -0,0 +1,10 @@
+package com.jinnova.smartpad.recom;
+
+import com.jinnova.smartpad.partner.PartnerManager;
+
+public class FavoriteAlertGenerator {
+
+	public static void main(String[] args) {
+		PartnerManager pm = new PartnerManager();
+	}
+}
