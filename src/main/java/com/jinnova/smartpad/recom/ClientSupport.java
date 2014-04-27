e6643875a04550506ec417bd6c49d24bc4cdce94
Huy Banh <huy.banh@jinnova.com>
2014-04-27 15:10:40 +0700

    generate data for drilling

---
+++ src/main/java/com/jinnova/smartpad/recom/ClientSupport.java
@@ -0,0 +1,110 @@
+package com.jinnova.smartpad.recom;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+
+import com.jinnova.smartpad.db.ScriptRunner;
+import com.jinnova.smartpad.partner.Catalog;
+import com.jinnova.smartpad.partner.PartnerManager;
+import com.jinnova.smartpad.partner.SmartpadCommon;
+import com.jinnova.smartpad.partner.SystemCatalogGenrator;
+
+public class ClientSupport {
+	
+	private final String drillDbhost;
+	private final String drillDbport;
+	private final String drillDbname;
+	private final String drillDblogin;
+	private final String drillDbpass;
+	
+	public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
+		
+		ClientSupport cs = new ClientSupport("localhost", null, "smartpad_drill", "root", "");
+		cs.buildDrillDatabase("smartpad");
+	}
+	
+	public void buildDrillDatabase(String mainDbname) throws SQLException, FileNotFoundException, IOException {
+		//initialize
+		dropDrillDatabaseIfExists();
+		ScriptRunner.createDatabase(drillDbhost, drillDbport, drillDbname, drillDblogin, drillDbpass, true);
+		SmartpadCommon.initialize(drillDbhost, drillDbport, drillDbname, drillDblogin, drillDbpass);
+		SystemCatalogGenrator.generate();
+		PartnerManager.loadSyscatsInitially();
+		copyDataToDrilling(mainDbname);
+	}
+
+	public ClientSupport(String drillDbhost, String drillDbport,
+			String drillDbname, String drillDblogin, String drillDbpass) {
+		super();
+		this.drillDbhost = drillDbhost;
+		this.drillDbport = drillDbport;
+		this.drillDbname = drillDbname;
+		this.drillDblogin = drillDblogin;
+		this.drillDbpass = drillDbpass;
+	}
+
+	private void copyDataToDrilling(String mainDbname) throws SQLException {
+
+		Connection conn = DriverManager.getConnection(makeDburl(drillDbhost, drillDbport, drillDbname), drillDblogin, drillDbpass);
+		Statement stmt = conn.createStatement();
+		String sql = "delete from catalogs";
+		System.out.println("SQL: " + sql);
+		stmt.executeUpdate(sql);
+		String[] tables = new String[] {"consumers", "catalogs", "operations", "promos"};
+		for (String oneTable : tables) {
+			sql = "insert into " + /*drillDbname + "." +*/ oneTable + " (select * from " + mainDbname + "." + oneTable + ")";
+			System.out.println("SQL: " + sql);
+			stmt.execute(sql);
+		}
+		
+		LinkedList<Catalog> catList = new LinkedList<>();
+		Catalog rootCat = PartnerManager.instance.getSystemRootCatalog();
+		catList.addAll(PartnerManager.instance.getSystemSubCatalog(rootCat.getId()));
+		while (!catList.isEmpty()) {
+			Catalog cat = catList.removeFirst();
+			sql = "insert into " + cat.getId() + " (select * from " + mainDbname + "." + cat.getId() + ")";
+			System.out.println("SQL: " + sql);
+			stmt.execute(sql);
+			LinkedList<Catalog> subCats = PartnerManager.instance.getSystemSubCatalog(cat.getId());
+			if (subCats != null) {
+				catList.addAll(subCats);
+			}
+		}
+		
+		conn.close();
+	}
+	
+	private void dropDrillDatabaseIfExists() throws SQLException {
+
+		Connection conn = DriverManager.getConnection(makeDburl(drillDbhost, drillDbport, "mysql"), drillDblogin, drillDbpass);
+		Statement stmt = conn.createStatement();
+		String sql = "drop database if exists " + drillDbname;
+		System.out.println("SQL: " + sql);
+		stmt.executeUpdate(sql);
+		stmt.close();
+		conn.close();
+	}
+	
+	private static String makeDburl(String dbhost, String dbport, String dbname) {
+		String dburl = "jdbc:mysql://" + dbhost;
+		if (dbport != null) {
+			dburl = dburl + ":" + dbport;
+		}
+		return dburl + "/" + dbname + "?useUnicode=true&characterEncoding=UTF-8";
+	}
+
+	/*private void generateOperationsClusters() throws SQLException {
+		DbIterator<String> consumers = new ConsumerDao().iterateConsumers();
+		DbIterator<Operation> operations = new OperationDao().iterateStores(branchId, excludeStoreId, offset, size);
+		OperationDao operDao = new OperationDao();
+		while (consumers.hasNext()) {
+			String one = consumers.next();
+			operDao.createOperation(operId, branchId, operation);
+		}
+	}*/
+}
